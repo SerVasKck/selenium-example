3986c6a2d904b29c7c33e9fa5f84345fd1fde8ea
SerVasKck <vasiliev@answerpro.ru>
2017-02-23 11:12:44 +0200

    update test

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
@@ -21,14 +24,15 @@ public class MyFirstTest {
     public void start(){
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
     }
 
     @Test
     public void myFirstTest(){
-        driver.get("https://github.com");
+        driver.get("https://www.google.ru/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btng")).click();
+        driver.findElement(By.name("btnG")).click();
         wait.until(titleIs("webdriver - Поиск в Google")) ;
     }
 
