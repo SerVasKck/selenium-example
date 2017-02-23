2b1e4cd99f04e7b5b6946526c064759cf947ae2f
SerVasKck <vasiliev@answerpro.ru>
2017-02-23 13:51:29 +0200

    all classes

---
+++ src/test/java/ru/stqa/training/selenium/Lesson2HomeTask3.java
@@ -0,0 +1,40 @@
+package ru.stqa.training.selenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+/**
+ * Created on 23.02.2017.
+ */
+public class Lesson2HomeTask3 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store")) ;
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/test/java/ru/stqa/training/selenium/Lesson2Part2Video11.java
@@ -0,0 +1,45 @@
+package ru.stqa.training.selenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+/**
+ * Created on 23.02.2017.
+ */
+public class Lesson2Part2Video11 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("https://www.google.ru/");
+        WebElement q =  driver.findElement(By.name("q"));
+        driver.navigate().refresh();
+        q.sendKeys("webdriver");
+
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google")) ;
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
